ab7debb160f6039e91fc965b9579a6985eff031c
Spirit Pearce (CEO of Cytanix) <spirit.pearce@gmail.com>
2025-11-15 11:32:46 +0000

    Update refactoring changes

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+#  © 2025 SpiritTheWalf/Cytanix. All rights reserved.
+#
+#  This file is part of the CynixBot project.
+#  The source code of this component is proprietary and confidential.
+#
+#  Unauthorized copying, modification, distribution, or use
+#  of this file, in whole or in part, is strictly prohibited.
+#
+#  Written by Spirit Pearce, 2025.
+#
